a64d32fcbc5c53df196decdc7a5668f02e4fe4a2
Gabriel Santos <gabriel.rodri.santos@gmail.com>
2024-03-12 16:58:19 -0300

    Gabarito 8 - 12

---
+++ Gabarito12/src/main/Java/Main.java
@@ -0,0 +1,11 @@
+package main.Java;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Cryptographye teste = new Cryptographye();
+
+        teste.encrypt("Felipe", 2);
+        teste.decrypt("Hgnkrg", 2);
+    }
+}
